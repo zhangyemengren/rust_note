63bbb763bbc084e0ad1f18971c18f23ee6e9623f
zhangyemengren <624593758@qq.com>
2023-09-15 14:08:31 +0800

    feat：add zero2prod

---
+++ zero_to_prod/actix_web/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, web, App, HttpServer, Responder, HttpRequest};
+
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
+}

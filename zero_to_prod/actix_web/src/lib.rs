f164eb1d6bd32ebc57aec140b2c52a873bee84e6
zhangyemengren <624593758@qq.com>
2023-09-18 14:07:09 +0800

    feat：next

--- zero_to_prod/actix_web/src/lib.rs
+++ zero_to_prod/actix_web/src/lib.rs
@@ -1,27 +1,30 @@
-use actix_web::{web, App, HttpServer, Responder, HttpRequest, HttpResponse};
+use std::net::TcpListener;
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
 
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
-pub fn run() -> Result<Server, std::io::Error> {
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
-
+            .route("/subscriptions", web::post().to(subscribe))
     })
-        .bind(("127.0.0.1", 8000))?
+        .listen(listener)?
         .run();
     Ok(server)
-
 }
--- zero_to_prod/actix_web/src/main.rs
+++ zero_to_prod/actix_web/src/main.rs
@@ -2,5 +2,6 @@ use zero_to_prod_actix_web::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = std::net::TcpListener::bind("127.0.0.1:0")?;
+    run(listener)?.await
 }

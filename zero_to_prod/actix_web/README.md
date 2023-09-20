4723c33207b4407224aa9092a996b913daa2300b
zhangyemengren <624593758@qq.com>
2023-09-20 17:56:07 +0800

    feat：新增docker启动脚本

---
+++ zero_to_prod/actix_web/README.md
@@ -0,0 +1,5 @@
+## scripts 文件夹下的脚本
+
+### init_db.sh
+启动 docker 数据库 postgresDB
+镜像为官方镜像 postgres
---
+++ zero_to_prod/actix_web/scripts/init_db.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+set -x
+set -eo pipefail
+
+#Check if custom user has been set, otherwise default to 'postgres'
+DB_USER=${POSTGRES_USER:=postgres}
+#Check if custom password has been set, otherwise default to 'password'
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+#Check if custom database name has been set, otherwise default to 'newsletter'
+DB_NAME="${POSTGRES_DB:=newsletter}"
+#Check if custom port has been set, otherwise default to '5432'
+DB_PORT="${POSTGRES_PORT:=5432}"
+#Check if custom host has been set, otherwise default to 'localhost'
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+#Launch postgres using Docker
+docker run \
+  --name postgres \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -h ${DB_HOST} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
